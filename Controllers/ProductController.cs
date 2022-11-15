59aede199779e302ca553a14b5d240d7c9e020a7
dwetterling <dwetterling@my.wctc.edu>
2022-11-15 11:17:33 -0600

    Discounts page, and discount links work

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Northwind.Models;
 using System.Linq;
@@ -9,6 +10,8 @@ namespace Northwind.Controllers
         // this controller depends on the NorthwindRepository
         private NorthwindContext _northwindContext;
         public ProductController(NorthwindContext db) => _northwindContext = db;
+        public ActionResult Discounts() => View(_northwindContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now));
+
         public IActionResult Category() => View(_northwindContext.Categories.OrderBy(c => c.CategoryName));
                 public IActionResult Index(int id){
             ViewBag.id = id;
--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -1,3 +1,26 @@
-$(function () {
+$(function(){
+    // preload audio
+    var toast = new Audio('toast.wav');
     
+    $(this).on('keydown', function(){
+        if (event.keyCode == 27){
+            $('#toast').toast('hide');
+        }
+    })
+    
+
+    $('.code').on('click', function (e) {
+        //alert($(this).data('product'));
+        // first pause the audio (in case it is still playing)
+        toast.pause();
+        // reset the audio
+        toast.currentTime = 0;
+        
+        e.preventDefault();
+         // play audio
+         toast.play();
+         $('#product').html($(this).data('product'));
+         $('#code').html($(this).data('code'));
+        $('#toast').toast({ autohide: false }).toast('show');
+    });
 });
