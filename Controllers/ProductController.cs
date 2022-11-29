5fb3561b56db2f417dcac5bf9c969513e80e7528
ian-fay <ianfay2003@gmail.com>
2022-11-29 10:35:22 -0600

    DIsplaying all discounts too employees

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -10,10 +10,19 @@ namespace Northwind.Controllers
         // this controller depends on the NorthwindRepository
         private NorthwindContext _northwindContext;
         public ProductController(NorthwindContext db) => _northwindContext = db;
-        public ActionResult Discounts() => View(_northwindContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now));
+        public ActionResult Discounts()
+        {
+            if (User?.Identity.IsAuthenticated == true)
+            {
+                if (User.IsInRole("northwind-employee")){
+                    return View(_northwindContext.Discounts);
+                }
+            }
+            return View(_northwindContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now));
+        }
 
         public IActionResult Category() => View(_northwindContext.Categories.OrderBy(c => c.CategoryName));
-                public IActionResult Index(int id){
+        public IActionResult Index(int id){
             ViewBag.id = id;
             return View(_northwindContext.Categories.OrderBy(c => c.CategoryName));
         }
